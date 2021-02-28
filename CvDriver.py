a5c9b5cb3266cd7a56e5256351f3760cef1ccb81
Stefan Titus <stefan.titus@keysight.com>
2021-02-28 12:16:32 -0800

    Update CV code to use a higher frequency and resolution

--- CvDriver.py
+++ CvDriver.py
@@ -1,25 +1,30 @@
 import libbcm2835._bcm2835 as soc
+import math
 
 class CvDriver:
     def __init__(self):
         # The PWM range, determines the resolution
-        self.range = 1024
+        self._range = 2 ** 16
         # The lowest MIDI note, for 0 V
-        self.zero_volt_note = 0
-        # The highest MIDI note
-        self.max_note = self.zero_volt_note + 12 * 4 + 1
-        # The PWM value to produce the highest note
-        self.max_note_value = self.range
+        self._zero_volt_note = 0 # not supported yet
+        
+        # The voltage at 100% duty cycle, found experimentally
+        self._max_voltage = 8.815
+
+        # Find the maximum producable note, which is the highest multiple of 1/12
+        self._max_note = math.floor(self._max_voltage * 12)
+        max_note_voltage = self._max_note / 12
+        self._max_note_value = math.floor(max_note_voltage / self._max_voltage * self._range)
 
         # Use GPIO 12, header pin 32, PWM0
         # Not sure why this isn't defined, but here's the value
         # pwm_pin = soc.RPI_V2_GPIO_P1_32 
         self.pwm_pin = 12
 
-        self.pwm_channel = 0
+        self._pwm_channel = 0
         
         # Use GPIO 17, header pin 11
-        self.gate_pin = soc.RPI_V2_GPIO_P1_11
+        self._gate_pin = 16
 
         ok = soc.bcm2835_init()
         if not ok:
@@ -30,48 +35,53 @@ class CvDriver:
 
 
         # Set the PWM clock divider
-        # 19.2 MHz / 64 = 300 kHz
-        soc.bcm2835_pwm_set_clock(soc.BCM2835_PWM_CLOCK_DIVIDER_64)
+        # 19.2 MHz / 2 = 9.6 MHz
+        soc.bcm2835_pwm_set_clock(soc.BCM2835_PWM_CLOCK_DIVIDER_2)
 
-        # Set PWM0 to Mark-Space mode (shouldn't matter), enabled
+        # TODO: Look at this!
+        # Set PWM0 to Balanced mode, enabled
         # Mark-Space mode is normal PWM
         # Balanced mode will distribute the 1s and 0s evenly to achieve a much higher switching frequency
         # This is cool, but probably not necessary.
         # For now, I want to see a normal PWM signal
-        soc.bcm2835_pwm_set_mode(self.pwm_channel, 0, True)
+        soc.bcm2835_pwm_set_mode(self._pwm_channel, 0, True)
 
-        # Set the PWM range to 1024, giving 10-bit resolution
-        # 19.2 MHz / 64 / 1024 = 293 Hz
-        soc.bcm2835_pwm_set_range(self.pwm_channel, self.range)
+        # Set the PWM range to 65536, giving 16-bit resolution
+        # 19.2 MHz / 2 / 65536 = 146 Hz, but balanced mode makes this ok
+        soc.bcm2835_pwm_set_range(self._pwm_channel, self._range)
 
         # Set the PWM output to 0
-        soc.bcm2835_pwm_set_data(self.pwm_channel, 0)
+        soc.bcm2835_pwm_set_data(self._pwm_channel, 0)
 
         # Set the gate pin to output
-        soc.bcm2835_gpio_fsel(self.gate_pin, soc.BCM2835_GPIO_FSEL_OUTP)
+        soc.bcm2835_gpio_fsel(self._gate_pin, soc.BCM2835_GPIO_FSEL_OUTP)
 
         # Set the gate pin low
-        soc.bcm2835_gpio_write(self.gate_pin, soc.LOW)
+        soc.bcm2835_gpio_write(self._gate_pin, soc.LOW)
 
     def __del__(self):
         # Set the PWM output to 0
-        soc.bcm2835_pwm_set_data(self.pwm_channel, 0)
+        soc.bcm2835_pwm_set_data(self._pwm_channel, 0)
         # Set the gate pin low
-        soc.bcm2835_gpio_write(self.gate_pin, soc.LOW)
+        soc.bcm2835_gpio_write(self._gate_pin, soc.LOW)
+        # Set the gate pin to high impedance
+        soc.bcm2835_gpio_fsel(self._gate_pin, soc.BCM2835_GPIO_FSEL_INPT)
+        
         soc.bcm2835_close()
 
     def note_on(self, note):
         '''note is a MIDI note number'''
-        value = (note - self.zero_volt_note) / (self.max_note - self.zero_volt_note) * self.max_note_value
-        value = min(value, self.range)
+        value = (note - self._zero_volt_note) / (self._max_note - self._zero_volt_note) * self._max_note_value
+        value = min(value, self._range)
         value = max(value, 0)
         value = int(value)
         # Set the PWM value
-        soc.bcm2835_pwm_set_data(self.pwm_channel, value)
+        soc.bcm2835_pwm_set_data(self._pwm_channel, value)
+        # print(value)
 
         # Set the gate pin high
-        soc.bcm2835_gpio_write(self.gate_pin, soc.HIGH)
+        soc.bcm2835_gpio_write(self._gate_pin, soc.HIGH)
 
     def note_off(self):
         # Set the gate pin low
-        soc.bcm2835_gpio_write(self.gate_pin, soc.LOW)
+        soc.bcm2835_gpio_write(self._gate_pin, soc.LOW)
